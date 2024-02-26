956f33e55e3c50df31943849c6e8c9747e0bde1c
tiller92 <ryantiller6@gmail.com>
2024-02-25 19:31:29 -0500

    Custom SVG might be the move

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import { useState , useEffect} from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import "./styles.css";
-import  PressureScaler from './components/PressureScaler';
-import LinePlot from "./components/LinePlot";
-import * as d3 from "d3";
-import  SimpleLineChart from './components/SimpleLineChart'; 
-import * as Plot from "@observablehq/plot";
+import MySVG from "./components/MySVG";
 
 function App() {
   const [current_pressure, setCurrentP] = useState(0);
@@ -36,15 +32,13 @@ function App() {
       }
     }
   
-  const [data, setData] = useState([{x: 1, y: 1}, {x:2,y: 2}]);
   return (
   <>
-      <SimpleLineChart data={data}/>
-      {/* <LinePlot data={data}/> */}
-    {/* <PressureScaler rows={xRange} cols={yRange} currentTime={currentTime} 
-    current_pressure={current_pressure}/> */}
-    <button className="btn"onClick={handleStart}>Ventilate</button>
-  </>
+    <MySVG cx={current_pressure} cy={currentTime}/>
+    <div>
+      <button className="btn btn-info" onClick={handleStart}>Ventilate</button>
+    </div>
+  </> 
 
   ) 
 };
